ebde79e788f5f5630c4d6d65d2b754ea264482c9
ancion <ancion@github.com>
2022-11-30 09:44:07 +0800

    remove coment

--- lua/lvim/core/alpha/dashboard.lua
+++ lua/lvim/core/alpha/dashboard.lua
@@ -100,17 +100,7 @@ function M.get_sections()
   }
 
   local text = require "lvim.interface.text"
-  local git_utils = require "lvim.utils.git"
-
-  local current_branch = git_utils.get_lvim_branch()
   local num_plugins_loaded = #vim.fn.globpath(get_runtime_dir() .. "/site/pack/packer/start", "*", 0, 1)
-
-  -- local lvim_version
-  -- if current_branch ~= "HEAD" or "" then
-  --   lvim_version = current_branch .. "-" .. git_utils.get_lvim_current_sha()
-  -- else
-  --   lvim_version = "v" .. git_utils.get_lvim_tag()
-  -- end
   local lvim_version = require("lvim.utils.git").get_lvim_version()
 
   local footer = {
@@ -131,42 +121,13 @@ function M.get_sections()
     },
   }
 
---   local status_ok, dashboard = pcall(require, "alpha.themes.dashboard")
---   if status_ok then
---     local function button(sc, txt, keybind, keybind_opts)
---       local b = dashboard.button(sc, txt, keybind, keybind_opts)
---       b.opts.hl_shortcut = "Include"
---       return b
---     end
-
---     buttons = {
---       val = {
---         button("f", lvim.icons.ui.FindFile .. " Find File", "<CMD>Telescope find_files<CR>"),
---         button("t", lvim.icons.ui.FindText .. "  Find Text", "<CMD>Telescope live_grep<CR>"),
---         button("n", lvim.icons.ui.NewFile .. "  New File", "<CMD>ene!<CR>"),
---         button("p", lvim.icons.ui.Aliens .. "  Recent Projects ", "<CMD>Telescope projects<CR>"),
---         button("r", lvim.icons.ui.FolderOpen .. "  Recent files", ":Telescope oldfiles <CR>"),
---         button(
---           "c",
---           lvim.icons.ui.Gear .. "  Configuration",
---           "<CMD>edit " .. require("lvim.config"):get_user_config_path() .. " <CR>"
---         ),
---         button("<Space> c", lvim.icons.ui.ColorPanel .. " Choose Colorschema ", "<CMD>Telescope colorscheme<CR>"),
---       },
---     },
---   return {
---     header = header,
---     buttons = buttons,
---     footer = footer,
---   }
--- end
   local buttons = {
     opts = {
       hl_shortcut = "Include",
       spacing = 1,
     },
     entries = {
-      { "f", lvim.icons.ui.FindFile .. "  Find File", "<CMD>Telescope find_files<CR>" },
+      { "f", lvim.icons.ui.FindFile .. " Find File", "<CMD>Telescope find_files<CR>" },
       { "t", lvim.icons.ui.FindText .. "  Find Text", "<CMD>Telescope live_grep<CR>" },
       { "n", lvim.icons.ui.NewFile .. "  New File", "<CMD>ene!<CR>" },
       { "p", lvim.icons.ui.Aliens .. "  Recent Projects ", "<CMD>Telescope projects<CR>" },
